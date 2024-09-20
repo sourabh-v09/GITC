e63e75a808854c0c5a9c2dd62ceb8e5a1046bf72
sourabh verma <159724825+sourabh-v09@users.noreply.github.com>
2024-09-20 12:27:13 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,5 @@
+age=int(input("enter the value of a"))
+if age>=5:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
---
+++ elif.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value of a"))
+#b=int(input("enter the value of b"))
+if a==5:
+    print("its equal")
+elif a>5:
+    print("it is more than 5")
+else:
+    print("its not equal")
